905b36dbb193313b52e0bb21bc3909d1a119b530
Alia Borroho <aliaborroho@gmail.com>
2015-09-17 15:11:10 -0500

    Updated sms templates and styles

--- app/digidoo/sms/create/route.js
+++ app/digidoo/sms/create/route.js
@@ -2,15 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-     this.store.createRecord('sms');
+     return this.store.createRecord('sms');
    },
 
    actions: {
-     submit: function(form) {
-     form.save().then(() => {
-     this.transitionTo('/');
-       });
-     }
+    submit: function(sms) {
+      sms.save().then(() => {
+        this.transitionTo('digidoo.sms.index');
+      });
+    }
    }
 });
 
