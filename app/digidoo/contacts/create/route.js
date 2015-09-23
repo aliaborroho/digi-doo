b2bf867bead39655369e0d3d15e27778e7e29365
Alia Borroho <aliaborroho@gmail.com>
2015-09-23 15:25:55 -0500

    fixing issues

--- app/digidoo/contacts/create/route.js
+++ app/digidoo/contacts/create/route.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 model: function() {
-   //return Ember.$.ajax('http://labortracker.azurewebsites.net/api/hospitals');
-   // Is this going to adapter.js?  Where in adapter.js?
    return this.store.createRecord('contact');
  },
+   //return Ember.$.ajax('http://labortracker.azurewebsites.net/api/hospitals');
+   // Is this going to adapter.js?  Where in adapter.js?
 
 
  actions: {
